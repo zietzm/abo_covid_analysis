2ac9f05846b4408e52008a6a59e50ae67d1ea0bd
Michael Zietz <michael.zietz@gmail.com>
2020-06-30 13:41:20 -0700

    Update UKB analysis while PCs compute

--- ukbb/compute_pcs.sh
+++ ukbb/compute_pcs.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+set -e
+
+rm -f /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/ukb_merged_list.txt
+
+# Add the file name to a running list (for easier merging below)
 for i in $(seq 1 22);
 do
-  plink \
-    --memory 20000 \
-    --bfile /data1/deep_storage/ukbiobank/pt2281/pfiles_wb/chr_bed_files/chr${i} \
-    --maf 0.05 \
-    --geno 0.01 \
-    --indep-pairwise 1000 kb 1 0.2 \
-    --make-bed \
-    --out /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/chr${i}
-
-  # Add the file name to a running list (for easier merging below)
-  printf "/data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/chr%s\n" $i >> /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/ukb_merged_list.txt
+  printf "/data1/deep_storage/ukbiobank/pt2281/pfiles_wb/chr_bed_files/chr${i}\n" $i >> /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/ukb_merged_list.txt
 done
 
 # Merge the files
@@ -23,19 +18,28 @@ plink \
   --make-bed \
   --out /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged
 
+plink \
+    --memory 20000 \
+    --bfile /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged \
+    --maf 0.05 \
+    --geno 0.01 \
+    --indep-pairwise 1000 kb 1 0.2 \
+    --make-bed \
+    --out /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged_filtered
+
 # Remove the chromosome-specific files (no longer needed)
-# rm /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/chr*
+rm /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged.*
 
 # Convert to Plink 2 for faster PCA (and other, later operations)
 plink2 \
   --memory 20000 \
-  --bfile /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged \
+  --bfile /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged_filtered \
   --make-pgen \
-  --out /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged
+  --out /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged_filtered
 
 # Compute PCs
 plink2 \
   --memory 20000 \
-  --pfile /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged \
+  --pfile /data1/home/mnz2108/data/ukbiobank/genotypes_for_pca/merged_filtered \
   --pca approx \
   --out /data1/home/mnz2108/data/ukbiobank/pca/pca
--- ukbb/extract_snp_genotypes.sh
+++ ukbb/extract_snp_genotypes.sh
@@ -1,59 +1,23 @@
-# # Get genotyped SNP genotypes
-# plink \
-#   --bfile /data1/deep_storage/ukbiobank/pt2281/pfiles_wb/chr_bed_files/chr9 \
-#   --mind 0.01 \
-#   --extract /data1/home/mnz2108/git/abo_covid_analysis/ukbb/snps.txt \
-#   --recode A \
-#   --out genotyped_genotypes
-
-# # Get information about the variants (ref/alt/position/etc.)
-# plink \
-#   --bfile /data1/deep_storage/ukbiobank/pt2281/pfiles_wb/chr_bed_files/chr9 \
-#   --extract /data1/home/mnz2108/git/abo_covid_analysis/ukbb/snps.txt \
-#   --make-just-bim \
-#   --out genotyped_genotypes
-
-# Get genotyped SNP genotypes
-plink \
-  --bed /data1/deep_storage/ukbiobank/bed_files/ukb_cal_chr9_v2.bed \
-  --bim /data1/home/mnz2108/data/ukbiobank/bim/ukb_snp_chr9_v2.bim \
-  --fam /data1/deep_storage/ukbiobank/genotypes/ukb41039_cal_chr9_v2_s488288.fam \
+# Get imputed SNP genotypes
+plink2 \
+  --bgen /data1/deep_storage/ukbiobank/imp_bgen_files/ukb_imp_chr9_v3.bgen ref-first \
+  --sample /data1/deep_storage/ukbiobank/genotypes/ukb41039_imp_chr9_v3_s487320.sample \
   --mind 0.01 \
-  --extract /data1/home/mnz2108/git/abo_covid_analysis/ukbb/snps.txt \
-  --recode A \
-  --out genotyped_genotypes
+  --extract snps.txt \
+  --export A \
+  --out imputed_genotypes
 
 # Get information about the variants (ref/alt/position/etc.)
-plink \
-  --bed /data1/deep_storage/ukbiobank/bed_files/ukb_cal_chr9_v2.bed \
-  --bim /data1/home/mnz2108/data/ukbiobank/bim/ukb_snp_chr9_v2.bim \
-  --fam /data1/deep_storage/ukbiobank/genotypes/ukb41039_cal_chr9_v2_s488288.fam \
+sqlite3 \
+  -header \
+  -csv /data1/deep_storage/ukbiobank/imp_bgi_files/004_ukb_imp_chr9_v3.bgen.bgi \
+  "SELECT * FROM Variant WHERE rsid IN ('rs8176746', 'rs8176719')" > \
+  imputed_variant_info.csv
+
+# Extract allele frequencies (to double check that we have variant direction correct (G > C))
+plink2 \
+  --bgen /data1/deep_storage/ukbiobank/imp_bgen_files/ukb_imp_chr9_v3.bgen ref-first \
+  --sample /data1/deep_storage/ukbiobank/genotypes/ukb41039_imp_chr9_v3_s487320.sample \
   --extract /data1/home/mnz2108/git/abo_covid_analysis/ukbb/snps.txt \
-  --make-just-bim \
-  --out genotyped_genotypes
-
-# # Get imputed SNP genotypes
-# plink2 \
-#   --bgen /data1/deep_storage/ukbiobank/imp_bgen_files/ukb_imp_chr9_v3.bgen ref-first \
-#   --sample /data1/deep_storage/ukbiobank/genotypes/ukb41039_imp_chr9_v3_s487320.sample \
-#   --mind 0.01 \
-#   --extract snps.txt \
-#   --export A \
-#   --out imputed_genotypes
-
-# # Get information about the variants (ref/alt/position/etc.)
-# sqlite3 \
-#   -header \
-#   -csv /data1/deep_storage/ukbiobank/imp_bgi_files/004_ukb_imp_chr9_v3.bgen.bgi \
-#   "SELECT * FROM Variant WHERE rsid IN ('rs8176746', 'rs8176719', 'rs687289', 'rs8176747', 'rs41302905')" > \
-#   imputed_variant_info.csv
-
-# # Extract allele frequencies (to double check that we have variant direction correct (G > C))
-# plink2 \
-#   --bgen /data1/deep_storage/ukbiobank/imp_bgen_files/ukb_imp_chr9_v3.bgen ref-first \
-#   --sample /data1/deep_storage/ukbiobank/genotypes/ukb41039_imp_chr9_v3_s487320.sample \
-#   --extract /data1/home/mnz2108/git/abo_covid_analysis/ukbb/snps.txt \
-#   --freq \
-#   --out /data1/home/mnz2108/git/abo_covid_analysis/ukbb/imputed_variants
-
-
+  --freq \
+  --out /data1/home/mnz2108/git/abo_covid_analysis/ukbb/imputed_variants
